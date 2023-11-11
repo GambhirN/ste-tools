b3be6b8127a53827d93058d0734e145372263ce8
Gambhir Nagaraju <ganagaraju@google.com>
2023-11-10 16:44:43 -0800

    test

--- test.sh
+++ test.sh
@@ -1,1 +1,3 @@
-echo "name=is_flag::hellonaga" >> "$GITHUB_OUTPUT"
+echo "is_avlo_requirements=hellonaga" >> "$GITHUB_OUTPUT"
+echo "is_avlo_testcases=tttttt" >> "$GITHUB_OUTPUT"
+echo "system_path=tester" >> "$GITHUB_OUTPUT"
