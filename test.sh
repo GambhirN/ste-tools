a760f0b4bdd1035039b3047bc5848f2545f90be3
Gambhir Nagaraju <ganagaraju@google.com>
2023-11-10 16:59:10 -0800

    test

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
-echo "is_avlo_requirements=true" >> "$GITHUB_OUTPUT"
+echo "is_avlo_requirements=\'true\'" >> "$GITHUB_OUTPUT"
 echo "is_avlo_testcases=tttttt" >> "$GITHUB_OUTPUT"
 echo "system_path=tester" >> "$GITHUB_OUTPUT"
